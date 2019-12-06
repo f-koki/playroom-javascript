e73b73d308d707d305afea2bb203903428d8740a
f-koki <57610613+f-koki@users.noreply.github.com>
2019-12-06 16:36:47 +0900

    add promise play

---
+++ promise.js
@@ -0,0 +1,9 @@
+const testExecutor = (resolve, reject) => {
+    setTimeout(()=>{
+        resolve('hoge')
+    }, 3000)
+}
+const testPromise = new Promise(testExecutor)
+testPromise.then(result => {
+    console.log(result)
+})
