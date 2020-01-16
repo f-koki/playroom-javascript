095ee48fcb5286df10e11efae4e5af19821914d9
f-koki <57610613+f-koki@users.noreply.github.com>
2020-01-16 15:33:36 +0900

    feat: add local scope test

--- src/scope/scope.js
+++ src/scope/scope.js
@@ -1,10 +1,18 @@
 /**
  * Global Scope
  */
-var globalScope = 'this variable is global';
+var globalScope = 'this variable is global scope';
 console.log(globalScope);
 
 var globalOutput = () => {
   console.log(globalScope);
 }
 globalOutput();
+
+/**
+ * Local Scope
+ */
+var localVariableDeclaration = () => {
+  var localVariable = 'this variable is local scope'
+}
+console.log(localvariable) // Error
