260976526a1cb337157a6ec1258a8a42d866442b
f-koki <57610613+f-koki@users.noreply.github.com>
2020-01-17 17:16:30 +0900

    feat: try to use syntax sugar of Promise reject

--- src/promise/promise-syntax-sugar.js
+++ src/promise/promise-syntax-sugar.js
@@ -11,3 +11,21 @@ resolvedPromise.then(value => {
 syntaxResolvedPromise.then(value => {
   console.log(value);
 });
+
+/**
+ * rejectのシンタクスシュガー
+ */
+var rejectedPromise = new Promise((resolve, reject) => { // 使わない"resolve"を記述する必要があり、無駄
+  reject(new Error('reject!!!'));
+});
+var syntaxRejectedPromise = Promise.reject(new Error('reject!!!'));
+rejectedPromise.then(value => {
+  console.log(value)
+}).catch(e => {
+  console.log(e.message);
+})
+syntaxRejectedPromise.then(value => {
+  console.log(value)
+}).catch(e => {
+  console.log(e.message);
+})
