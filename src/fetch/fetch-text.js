7bfb2ca5987675fcb2ab2f16dbb3576c1820521e
f-koki <57610613+f-koki@users.noreply.github.com>
2020-01-23 19:23:42 +0900

    refactor get zipcode from html form

--- src/fetch/fetch-text.js
+++ src/fetch/fetch-text.js
@@ -1,15 +1,20 @@
 /**
  * 試すのにMoesif Orign & CORS Changerが必要（CORSを無効化するChrome Extension）
  */
-// TODO: zipcodeをformから受け取れるようにする
-var zipcode = 1670051;
-var zipcloudUrl = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + zipcode;
-var exportTarget = document.getElementById("fetch-test");
-console.log(exportTarget);
-fetch(zipcloudUrl)
-  .then(res => res.json())
-  .then(json => {
-    if (json) {
-      exportTarget.innerHTML = JSON.stringify(json);
-    }
-  });
+const getInfo = () => {
+  var zipcode = document.myform.postcode.value;
+  var zipcloudUrl = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + zipcode;
+  var exportTarget = document.getElementById("fetch-test");
+  fetch(zipcloudUrl)
+    .then(res => {
+      res.json()
+    })
+    .then(json => {
+      if (json) {
+        exportTarget.innerHTML = JSON.stringify(json);
+      }
+    })
+    .catch(e => {
+      alert('failed')
+    });
+}
